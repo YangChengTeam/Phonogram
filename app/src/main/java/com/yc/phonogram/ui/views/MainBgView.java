389c18bad2468c3750419867b637f798a583da13
mzpbvsig <972403325@qq.com>
2017-12-18 19:05:37 +0800

    fix: MainBgView.java

--- app/src/main/java/com/yc/phonogram/ui/views/MainBgView.java
+++ app/src/main/java/com/yc/phonogram/ui/views/MainBgView.java
@@ -1,15 +1,24 @@
 package com.yc.phonogram.ui.views;
 
+import android.app.Fragment;
 import android.content.Context;
 import android.util.AttributeSet;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.FrameLayout;
 import android.widget.ImageView;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
 
+import com.jakewharton.rxbinding.view.RxView;
+import com.kk.utils.ScreenUtil;
 import com.yc.phonogram.R;
 
 import org.w3c.dom.Text;
+
+import java.util.concurrent.TimeUnit;
+
+import rx.functions.Action1;
 
 /**
  * Created by zhangkai on 2017/12/18.
@@ -21,7 +30,7 @@ public class MainBgView extends BaseView {
     private ImageView mLeftImageView;
     private ImageView mRightImageView;
     private TextView mIndexTextView;
-
+    private FrameLayout mContextFrameLayout;
 
     public MainBgView(Context context, AttributeSet attrs) {
         super(context, attrs);
@@ -36,7 +45,6 @@ public class MainBgView extends BaseView {
         return R.layout.view_main_bg;
     }
 
-
     @Override
     public void init() {
         mIndexRelativeLayout = (RelativeLayout) getView(R.id.rl_index);
@@ -44,6 +52,25 @@ public class MainBgView extends BaseView {
         mLeftImageView = (ImageView) getView(R.id.iv_left);
         mRightImageView = (ImageView) getView(R.id.iv_right);
         mIndexTextView = (TextView) getView(R.id.tv_index);
+        mContextFrameLayout = (FrameLayout) getView(R.id.fl_content);
+
+        RxView.clicks(mLeftImageView).throttleFirst(200, TimeUnit.MILLISECONDS).subscribe(new Action1<Void>() {
+            @Override
+            public void call(Void aVoid) {
+                if (indexListener != null) {
+                    indexListener.leftClick(mLeftImageView);
+                }
+            }
+        });
+
+        RxView.clicks(mRightImageView).throttleFirst(200, TimeUnit.MILLISECONDS).subscribe(new Action1<Void>() {
+            @Override
+            public void call(Void aVoid) {
+                if (indexListener != null) {
+                    indexListener.rightClcik(mRightImageView);
+                }
+            }
+        });
     }
 
     private int count;
@@ -75,5 +102,17 @@ public class MainBgView extends BaseView {
 
     public void showInnerBg() {
         mInnerBgImageView.setVisibility(View.VISIBLE);
+    }
+
+    private IndexListener indexListener;
+
+    public void setIndexListener(IndexListener indexListener) {
+        this.indexListener = indexListener;
+    }
+
+    public interface IndexListener {
+        void leftClick(View view);
+
+        void rightClcik(View view);
     }
 }
