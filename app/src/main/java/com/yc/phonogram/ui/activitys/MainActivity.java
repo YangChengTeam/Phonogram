c31b53e067e0d2abe66367dec7bf245faf030f93
2037097758@qq.com <2037097758@qq.com>
2017-12-18 14:37:40 +0800

    add share ui

--- app/src/main/java/com/yc/phonogram/ui/activitys/MainActivity.java
+++ app/src/main/java/com/yc/phonogram/ui/activitys/MainActivity.java
@@ -1,8 +1,12 @@
 package com.yc.phonogram.ui.activitys;
 
 import android.os.Bundle;
+import android.view.Gravity;
+import android.view.View;
 
+import com.kk.utils.ScreenUtil;
 import com.yc.phonogram.R;
+import com.yc.phonogram.ui.popupwindow.SharePopupWindow;
 
 
 public class MainActivity extends BaseActivity {
@@ -13,7 +17,14 @@ public class MainActivity extends BaseActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+//
 
+    }
+
+    public void share(View view) {
+        SharePopupWindow sharePopupWindow = new SharePopupWindow(this);
+        sharePopupWindow.showAtLocation(findViewById(R.id.container), Gravity.CENTER, 0, 0);
+//        sharePopupWindow.setWidth(ScreenUtil.getWidth(this));
     }
 
 }
--- app/src/main/java/com/yc/phonogram/ui/popupwindow/SharePopupWindow.java
+++ app/src/main/java/com/yc/phonogram/ui/popupwindow/SharePopupWindow.java
@@ -2,6 +2,8 @@ package com.yc.phonogram.ui.popupwindow;
 
 import android.app.Activity;
 
+import com.yc.phonogram.R;
+
 /**
  * Created by zhangkai on 2017/12/15.
  */
@@ -13,6 +15,7 @@ public class SharePopupWindow extends BasePopupWindow {
 
     @Override
     public int getLayoutID() {
-        return 0;
+
+        return R.layout.popwindow_share_view;
     }
 }
