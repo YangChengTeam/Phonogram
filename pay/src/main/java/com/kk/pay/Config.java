5eb377e80007f17733803d46f5bea3bc486c6f0d
2037097758@qq.com <2037097758@qq.com>
2017-12-20 10:51:00 +0800

    提交支付

--- pay/src/main/java/com/kk/pay/Config.java
+++ pay/src/main/java/com/kk/pay/Config.java
@@ -1,4 +1,4 @@
-package com.yc.phonogram.domain;
+package com.kk.pay;
 
 /**
  * Created by zhangkai on 2017/10/19.
@@ -15,19 +15,7 @@ public class Config {
 
     public static final String APPID = "?app_id=5";
 
-
-    public static final String INIT_URL = getBaseUrl() + "index/init" + APPID;
-    public static final String ORDER_URL = getBaseUrl() + "index/pay" + APPID;
-    public static final String VIP_LIST_URL = getBaseUrl() + "index/vip_list" + APPID;
     public final static String CHECK_URL = getBaseUrl() + "index/orders_check" + APPID;
-    public final static String QUERY_URL = getBaseUrl() + "index/orders_query" + APPID;
-    public static final String PAY_WAY_LIST_URL = getBaseUrl() + "index/payway_list" + APPID;
-    public static final String TYPE_LIST_URL = getBaseUrl() + "index/vip_flag_list" + APPID;
-
-    public static final String PHONOGRAM_LIST_URL = getBaseUrl() + "index/phonetic_list" + APPID;
-    public static final String MCLASS_LIST_URL = getBaseUrl() + "index/phonetic_class" + APPID;
-
-
 
     public static String getBaseUrl() {
         String baseUrl = "http://tic.upkao.com/api/";
