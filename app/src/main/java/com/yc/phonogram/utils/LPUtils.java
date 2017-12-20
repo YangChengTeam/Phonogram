42bde32322f89d0bcab0e95a0b8c28ea8b9fe15b
Yuye584312311 <584312311@qq.com>
2017-12-20 15:31:10 +0800

    edit lp

--- app/src/main/java/com/yc/phonogram/utils/LPUtils.java
+++ app/src/main/java/com/yc/phonogram/utils/LPUtils.java
@@ -1,5 +1,10 @@
 package com.yc.phonogram.utils;
 
+
+import android.text.TextUtils;
+import android.util.Log;
+import java.util.regex.Pattern;
+
 /**
  * TinyHung@Outlook.com
  * 2017/12/20.
@@ -7,4 +12,50 @@ package com.yc.phonogram.utils;
 
 public class LPUtils {
 
+    private static final String TAG = LPUtils.class.getSimpleName();
+
+    /**
+     * 给单词的双舌音部分加上颜色
+     * @param word 源单词
+     * @param letter 双舌音部分
+     * @return
+     */
+    public static String addWordLetterColor(String word, String letter) {
+        if(TextUtils.isEmpty(word))return word;
+        if(TextUtils.isEmpty(letter))return word;
+        if(word.contains(letter)){
+            return word.replace(letter,"<font color='#FD0000'>"+letter);
+        }
+        return word;
+    }
+
+    /**
+     * 给音标加上颜色
+     * @param phonetic 源音标
+     * @param letter
+     * @return
+     */
+    public static String addWordPhoneticLetterColor(String phonetic, String letter) {
+
+        if(TextUtils.isEmpty(phonetic))return phonetic;
+        if(TextUtils.isEmpty(letter))return phonetic;
+
+        if(null==phonetic) return phonetic;
+        Pattern pattern = Pattern.compile("/");
+        String[] strs = pattern.split(phonetic);
+        if(null!=strs&&strs.length>0){
+            StringBuffer stringBuffer=new StringBuffer();
+            for (String str : strs) {
+                Log.d(TAG,"str="+str);
+                if(!TextUtils.equals("/",str)){
+                    stringBuffer.append(str);
+                }
+            }
+            Log.d(TAG,"stringBuffer="+stringBuffer);
+            if(phonetic.contains(stringBuffer.toString())){
+                return phonetic.replace(stringBuffer.toString(),"<font color='#FD0000'>"+stringBuffer.toString());
+            }
+        }
+        return phonetic;
+    }
 }
