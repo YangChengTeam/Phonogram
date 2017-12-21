020356eac4ab9d898ca883dd3714be690269a78c
myflying <512710257@qq.com>
2017-12-21 14:11:33 +0800

    fix

---
+++ video-player/build/generated/source/buildConfig/debug/com/xinqu/videoplayer/BuildConfig.java
@@ -0,0 +1,13 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.xinqu.videoplayer;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "com.xinqu.videoplayer";
+  public static final String BUILD_TYPE = "debug";
+  public static final String FLAVOR = "";
+  public static final int VERSION_CODE = 72;
+  public static final String VERSION_NAME = "6.2.1";
+}
