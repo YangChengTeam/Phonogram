87a2e553759df14129c0c3ea04bbfe3ff6494bad
sunshey <2037097758@qq.com>
2019-01-25 13:47:52 +0800

    音标保存

--- app/src/main/java/com/yc/phonogram/ui/activitys/SplashActivity.java
+++ app/src/main/java/com/yc/phonogram/ui/activitys/SplashActivity.java
@@ -1,60 +1,47 @@
 package com.yc.phonogram.ui.activitys;
 
-import android.Manifest;
-import android.annotation.TargetApi;
-import android.app.Activity;
 import android.content.Intent;
-import android.content.pm.PackageManager;
 import android.media.MediaPlayer;
-import android.net.Uri;
-import android.os.Build;
 import android.os.Handler;
-import android.provider.Settings;
 import android.support.v4.content.ContextCompat;
-import android.util.Log;
 import android.view.KeyEvent;
 import android.view.View;
-import android.view.ViewGroup;
 import android.widget.FrameLayout;
 import android.widget.ImageView;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
-import android.widget.Toast;
 
-import com.qq.e.ads.splash.SplashAD;
-import com.qq.e.ads.splash.SplashADListener;
-import com.qq.e.comm.util.AdError;
-import com.yc.phonogram.App;
+import com.qq.e.ads.nativ.NativeExpressADView;
 import com.yc.phonogram.R;
 import com.yc.phonogram.domain.Config;
 import com.yc.phonogram.utils.Mp3Utils;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.Map;
 import java.util.concurrent.TimeUnit;
 
 import rx.Observable;
 import rx.Subscription;
 import rx.android.schedulers.AndroidSchedulers;
 import rx.functions.Action1;
+import yc.com.tencent_adv.AdvDispatchManager;
+import yc.com.tencent_adv.AdvType;
+import yc.com.tencent_adv.OnAdvStateListener;
 
 /**
  * Created by zhangkai on 2017/12/15.
  */
 
-public class SplashActivity extends BaseActivity implements SplashADListener {
+public class SplashActivity extends BaseActivity implements  OnAdvStateListener {
     private Subscription subscription = null;
     private MediaPlayer mediaPlayer;
     public static SplashActivity INSTANCE;
-    Handler handler=new Handler();
+    Handler handler = new Handler();
 
     private FrameLayout container;
     private TextView skipView;
     private ImageView logoImageView;
     private RelativeLayout relativeLayout;
-    private long fetchSplashADTime;
-    private boolean canJump;
-    private long minSplashTimeWhenNoAD=2000;
+    private long minSplashTimeWhenNoAD = 2000;
 
     @Override
     public int getLayoutId() {
@@ -64,17 +51,13 @@ public class SplashActivity extends BaseActivity implements SplashADListener {
     @Override
     public void init() {
         INSTANCE = this;
-         logoImageView = findViewById(R.id.iv_logo);
+        logoImageView = findViewById(R.id.iv_logo);
         container = findViewById(R.id.splash_container);
         skipView = findViewById(R.id.skip_view);
         relativeLayout = findViewById(R.id.retry_layout);
-        // 如果targetSDKVersion >= 23，就要申请好权限。如果您的App没有适配到Android6.0（即targetSDKVersion < 23），那么只需要在这里直接调用fetchSplashAD接口。
-        if (Build.VERSION.SDK_INT >= 23) {
-            checkAndRequestPermission();
-        } else {
-            // 如果是Android6.0以下的机器，默认在安装时获得了所有权限，可以直接调用SDK
-            fetchSplashAD(this, container, skipView, Config.AV_APPID, Config.AV_SPLASH_ID, this, 0);
-        }
+
+        AdvDispatchManager.getManager().init(this, AdvType.SPLASH, container, skipView, Config.AV_APPID, Config.AV_SPLASH_ID, this);
+
         final Integer[] bgIDs = new Integer[]{R.mipmap.splash_bg1, R.mipmap.splash_bg2, R.mipmap.splash_bg3, R.mipmap
                 .splash_bg4};
         subscription = Observable.interval(300, TimeUnit.MILLISECONDS).observeOn
@@ -100,149 +83,21 @@ public class SplashActivity extends BaseActivity implements SplashADListener {
                     public void run() {
                         startActivity(new Intent(SplashActivity.this, MainActivity.class));
                     }
-                },500);
+                }, 500);
 
             }
         });
 
     }
 
-    /**
-     * ----------非常重要----------
-     * <p>
-     * Android6.0以上的权限适配简单示例：
-     * <p>
-     * 如果targetSDKVersion >= 23，那么必须要申请到所需要的权限，再调用广点通SDK，否则广点通SDK不会工作。
-     * <p>
-     * Demo代码里是一个基本的权限申请示例，请开发者根据自己的场景合理地编写这部分代码来实现权限申请。
-     * 注意：下面的`checkSelfPermission`和`requestPermissions`方法都是在Android6.0的SDK中增加的API，如果您的App还没有适配到Android6.0以上，则不需要调用这些方法，直接调用广点通SDK即可。
-     */
-    @TargetApi(Build.VERSION_CODES.M)
-    private void checkAndRequestPermission() {
-        List<String> lackedPermission = new ArrayList<String>();
-        if (!(checkSelfPermission(Manifest.permission.READ_PHONE_STATE) == PackageManager.PERMISSION_GRANTED)) {
-            lackedPermission.add(Manifest.permission.READ_PHONE_STATE);
-        }
-
-        if (!(checkSelfPermission(Manifest.permission.WRITE_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED)) {
-            lackedPermission.add(Manifest.permission.WRITE_EXTERNAL_STORAGE);
-        }
-
-        if (!(checkSelfPermission(Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED)) {
-            lackedPermission.add(Manifest.permission.ACCESS_FINE_LOCATION);
-        }
-
-        // 权限都已经有了，那么直接调用SDK
-        if (lackedPermission.size() == 0) {
-            fetchSplashAD(this, container, skipView, Config.AV_APPID, Config.AV_SPLASH_ID, this, 0);
-        } else {
-            // 请求所缺少的权限，在onRequestPermissionsResult中再看是否获得权限，如果获得权限就可以调用SDK，否则不要调用SDK。
-            String[] requestPermissions = new String[lackedPermission.size()];
-            lackedPermission.toArray(requestPermissions);
-            requestPermissions(requestPermissions, 1024);
-        }
-    }
-
-    private boolean hasAllPermissionsGranted(int[] grantResults) {
-        for (int grantResult : grantResults) {
-            if (grantResult == PackageManager.PERMISSION_DENIED) {
-                return false;
-            }
-        }
-        return true;
-    }
 
     @Override
     public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
         super.onRequestPermissionsResult(requestCode, permissions, grantResults);
-        if (requestCode == 1024 && hasAllPermissionsGranted(grantResults)) {
-            fetchSplashAD(this, container, skipView, Config.AV_APPID, Config.AV_SPLASH_ID, this, 0);
-        } else {
-            // 如果用户没有授权，那么应该说明意图，引导用户去设置里面授权。
-            Toast.makeText(this, "应用缺少必要的权限！请点击\"权限\"，打开所需要的权限。", Toast.LENGTH_LONG).show();
-            Intent intent = new Intent(Settings.ACTION_APPLICATION_DETAILS_SETTINGS);
-            intent.setData(Uri.parse("package:" + getPackageName()));
-            startActivity(intent);
-            finish();
-        }
+
+        AdvDispatchManager.getManager().onRequestPermissionsResult(requestCode, permissions, grantResults);
     }
 
-
-    /**
-     * 拉取开屏广告，开屏广告的构造方法有3种，详细说明请参考开发者文档。
-     *
-     * @param activity      展示广告的activity
-     * @param adContainer   展示广告的大容器
-     * @param skipContainer 自定义的跳过按钮：传入该view给SDK后，SDK会自动给它绑定点击跳过事件。SkipView的样式可以由开发者自由定制，其尺寸限制请参考activity_splash.xml或者接入文档中的说明。
-     * @param appId         应用ID
-     * @param posId         广告位ID
-     * @param adListener    广告状态监听器
-     * @param fetchDelay    拉取广告的超时时长：取值范围[3000, 5000]，设为0表示使用广点通SDK默认的超时时长。
-     */
-    private void fetchSplashAD(Activity activity, ViewGroup adContainer, View skipContainer,
-                               String appId, String posId, SplashADListener adListener, int fetchDelay) {
-        fetchSplashADTime = System.currentTimeMillis();
-        SplashAD splashAD = new SplashAD(activity, adContainer, skipContainer, appId, posId, adListener, fetchDelay);
-    }
-
-    @Override
-    public void onADPresent() {
-        Log.i("AD_DEMO", "SplashADPresent");
-        relativeLayout.setVisibility(View.GONE); // 广告展示后一定要把预设的开屏图片隐藏起来
-    }
-
-    @Override
-    public void onADClicked() {
-        Log.i("AD_DEMO", "SplashADClicked");
-    }
-
-    /**
-     * 倒计时回调，返回广告还将被展示的剩余时间。
-     * 通过这个接口，开发者可以自行决定是否显示倒计时提示，或者还剩几秒的时候显示倒计时
-     *
-     * @param millisUntilFinished 剩余毫秒数
-     */
-    @Override
-    public void onADTick(long millisUntilFinished) {
-        Log.i("AD_DEMO", "SplashADTick " + millisUntilFinished + "ms");
-        skipView.setText(String.format(getString(R.string.click_to_skip), Math.round(millisUntilFinished / 1000f)));
-    }
-
-    @Override
-    public void onADExposure() {
-        Log.i("AD_DEMO", "SplashADExposure");
-    }
-
-    @Override
-    public void onADDismissed() {
-        Log.i("AD_DEMO", "SplashADDismissed");
-//        next();
-        if (canJump) {
-            switchMain(minSplashTimeWhenNoAD);
-        } else {
-            canJump = true;
-        }
-    }
-
-    @Override
-    public void onNoAD(AdError error) {
-        Log.i(
-                "AD_DEMO",
-                String.format("LoadSplashADFail, eCode=%d, errorMsg=%s", error.getErrorCode(),
-                        error.getErrorMsg()));
-        /**
-         * 为防止无广告时造成视觉上类似于"闪退"的情况，设定无广告时页面跳转根据需要延迟一定时间，demo
-         * 给出的延时逻辑是从拉取广告开始算开屏最少持续多久，仅供参考，开发者可自定义延时逻辑，如果开发者采用demo
-         * 中给出的延时逻辑，也建议开发者考虑自定义minSplashTimeWhenNoAD的值
-         **/
-        final long alreadyDelayMills = System.currentTimeMillis() - fetchSplashADTime;//从拉广告开始到onNoAD已经消耗了多少时间
-        if (mediaPlayer != null) {
-            mediaPlayer.start();
-        }
-        switchMain(alreadyDelayMills);
-
-
-    }
 
     private void switchMain(long delay) {
         long delayTime = 0;
@@ -268,7 +123,8 @@ public class SplashActivity extends BaseActivity implements SplashADListener {
     @Override
     protected void onPause() {
         super.onPause();
-        canJump=false;
+//        canJump = false;
+        AdvDispatchManager.getManager().onPause();
         if (mediaPlayer != null) {
             mediaPlayer.stop();
         }
@@ -277,10 +133,8 @@ public class SplashActivity extends BaseActivity implements SplashADListener {
     @Override
     protected void onResume() {
         super.onResume();
-        if (canJump){
-            switchMain(fetchSplashADTime);
-        }
-        canJump=true;
+        AdvDispatchManager.getManager().onResume();
+
     }
 
     @Override
@@ -302,4 +156,34 @@ public class SplashActivity extends BaseActivity implements SplashADListener {
         }
         return super.onKeyDown(keyCode, event);
     }
+
+    @Override
+    public void onShow() {
+        relativeLayout.setVisibility(View.GONE); // 广告展示后一定要把预设的开屏图片隐藏起来
+    }
+
+    @Override
+    public void onDismiss(long delayTime) {
+        if (mediaPlayer != null) {
+            mediaPlayer.start();
+        }
+        switchMain(delayTime);
+    }
+
+    @Override
+    public void onError() {
+
+    }
+
+    @Override
+    public void onNativeExpressDismiss(NativeExpressADView view) {
+
+    }
+
+    @Override
+    public void onNativeExpressShow(Map<NativeExpressADView, Integer> mDatas) {
+
+    }
+
+
 }
